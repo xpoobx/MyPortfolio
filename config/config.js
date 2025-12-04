16d9f8061979fc3004b376563ff9c682981db5b5
xpoobx <dantepellico@gmail.com>
2025-12-04 17:31:15 -0500

    revert

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  port: process.env.PORT,
+  port: process.env.PORT || 3000,
   mongoUri: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
 };
